05057a94088045ed30a9c9aada2c7aaaff599ef5
Michael Brauweiler <michael.brauweiler@posteo.de>
2019-04-13 10:40:08 +0200

    Add basic tests and fixture

--- surveys/urls.py
+++ surveys/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('<int:thesis_id>/', views.thesis),
-    path('<int:thesis_id>/<int:stance>/', views.stance),
-    path('result/', views.evaluation),
+    path('', views.index, name='index'),
+    path('<int:thesis_id>/', views.thesis, name='theses'),
+    path('<int:thesis_id>/<int:stance>/', views.stance, name='stances'),
+    path('result/', views.evaluation, name='result'),
 ]
