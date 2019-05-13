2bd24d586a64712d087c95171a4021f4d483f508
Simon Ellmann <simon.ellmann@tum.de>
2019-05-13 23:27:16 +0200

    Update project for deployment

--- walomat/settings.py
+++ walomat/settings.py
@@ -140,7 +140,25 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/uploads/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 
+if 'DJANGO_PRODUCTION' in os.environ:
+    ALLOWED_HOSTS = ['*']
+    DEBUG = False
+
+    SECRET_KEY = os.environ['SECRET_KEY']
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ['DATABASE_NAME'],
+            'USER': os.environ['DATABASE_USERNAME'],
+            'PASSWORD': os.environ['DATABASE_PASSWORD'],
+            'HOST': os.environ['DATABASE_HOST'],
+            'PORT': '',
+        }
+    }
+
 if DEBUG:
     STATICFILES_DIRS = [STATIC_DIR]
 else:
     STATIC_ROOT = STATIC_DIR
+
